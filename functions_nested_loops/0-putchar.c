86721f15fbe9843d453a585c4b2458a28fd38883
Dalouba <dalou224@live.fr>
2023-10-18 05:11:55 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,20 @@
+#include "main.h"
 #include <stdio.h>
-
+/**
+ * main - program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
 	return (0);
 }
