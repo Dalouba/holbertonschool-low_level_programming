ae1d8e8b8b84e0afe4a50a61f766a6eb539c3587
Dalouba <dalou224@live.fr>
2023-10-18 10:47:16 -0700

    print alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alpahbet_x10 - Prints 10 times the alphabet, in lowcase.
+ *
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+
+			_putchar(i);
+		}
+			_putchar('\n');
+	}
+}
+
+
