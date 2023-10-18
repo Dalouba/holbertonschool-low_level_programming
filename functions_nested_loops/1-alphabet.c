79b10e4a4c832899d426503f9a6ab290ffe48a9c
Dalouba <dalou224@live.fr>
2023-10-18 06:20:15 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int void print_alphabet(void);
+{
+	    int i;
+	    for (i >= 97; i <= 122; i++)
+	    {
+		    print_alphabet ();
+		}
+	        return (0);
+}
