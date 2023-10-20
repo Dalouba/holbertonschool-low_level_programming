03b3329021fbccfe8f76fc5a84ba31cc8c857190
Dalouba <dalou224@live.fr>
2023-10-20 05:42:37 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - check for a integer
+ * @r: The character to be checked
+ ** Return: the absolute value of an integer.
+ */
+int _abs(int r)
+{
+	return (abs(r));
+}
+
