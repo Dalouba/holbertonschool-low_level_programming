4e82433467767cad5f35abb8712cf264de977643
Dalouba <dalou224@live.fr>
2023-10-24 03:43:22 -0700

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
@@ -11,8 +11,8 @@ void _puts(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
