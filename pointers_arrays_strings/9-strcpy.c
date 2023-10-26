dd9422a8df91a8979de73201f59638b3edd34f9d
Dalouba <dalou224@live.fr>
2023-10-26 04:43:42 -0700

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * char *_strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest.
  * @dest: pointer to the buffer in which you copy the string
