d1b8217104c1d0c35c0f703ab353b4700641efc6
Dalouba <dalou224@live.fr>
2023-10-27 04:31:01 -0700

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,19 +13,19 @@ char *_strcat(char *dest, char *src)
 	int j = 0;
 
 	while (dest[i] != '\0')
-		{
-			i++;
-		}
+	{
+		i++;
+	}
 
 	while (src[j] != '\0')
-		{
-			dest[i + j] = src[j];
-			j++;
-		}
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
 
-		
-	dest [i + j] = '\0';
-		
+
+	dest[i + j] = '\0';
+
 
 	return (dest);
 
